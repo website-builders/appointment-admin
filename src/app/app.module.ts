fe9a611968fccdb5f0d7fbdd30e14e4a7fbfd982
Trinethra Reddy <trinethra.a@techaspect.com>
2020-03-01 14:00:03 +0530

    added fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
