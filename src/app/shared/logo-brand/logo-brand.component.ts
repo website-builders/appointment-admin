9463f36e34fdd9d10050ef402acbef70cc1a9c27
Trinethra Reddy <trinethra.a@techaspect.com>
2020-03-07 23:59:42 +0530

    added MDI icons, added welcome page

---
+++ src/app/shared/logo-brand/logo-brand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-brand',
+  templateUrl: './logo-brand.component.html',
+  styleUrls: ['./logo-brand.component.scss']
+})
+export class LogoBrandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoBrandComponent } from './logo-brand/logo-brand.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ServerDownComponent } from './server-down/server-down.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LogoBrandComponent,
+    WelcomeComponent,
+    PageNotFoundComponent,
+    ServerDownComponent
+  ],
   imports: [
     CommonModule
   ],
   exports: [
+    LogoBrandComponent,
+    WelcomeComponent,
+    PageNotFoundComponent,
+    ServerDownComponent
   ]
 })
 export class SharedModule { }
